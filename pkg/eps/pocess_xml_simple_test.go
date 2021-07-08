14a34d1c8034c1b81589968651b2fab592896a39
Seb <git@erhardt.net>
2021-07-08 11:02:02 +0200

    add more tests

--- pkg/eps/pocess_xml_simple_test.go
+++ pkg/eps/pocess_xml_simple_test.go
@@ -1,7 +1,6 @@
 package eps
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
@@ -13,5 +12,24 @@ func TestProcessXMLSimple(t *testing.T) {
 	ass.NoError(err)
 	patDoc, err := ProcessXMLSimple(data)
 	ass.NoError(err)
-	fmt.Println(patDoc)
+	ass.Equal("de", patDoc.Title[0].Language)
+	ass.Equal("BREMSÜBERWACHUNG", patDoc.Title[0].Text)
+	ass.Equal("en", patDoc.Title[1].Language)
+	ass.Equal("BRAKE MONITORING", patDoc.Title[1].Text)
+	ass.Equal("fr", patDoc.Title[2].Language)
+	ass.Equal("SURVEILLANCE DE FREIN", patDoc.Title[2].Text)
+}
+
+func TestProcessXMLSimple2(t *testing.T) {
+	ass := assert.New(t)
+	data, err := ioutil.ReadFile("../../dtds/1-5-1.xml")
+	ass.NoError(err)
+	patDoc, err := ProcessXMLSimple(data)
+	ass.NoError(err)
+	ass.Equal("de", patDoc.Title[0].Language)
+	ass.Equal("KOMMUNIKATIONSVORRICHTUNG UND -VERFAHREN", patDoc.Title[0].Text)
+	ass.Equal("en", patDoc.Title[1].Language)
+	ass.Equal("COMMUNICATIONS DEVICE AND METHOD", patDoc.Title[1].Text)
+	ass.Equal("fr", patDoc.Title[2].Language)
+	ass.Equal("DISPOSITIF ET PROCÉDÉ DE COMMUNICATIONS", patDoc.Title[2].Text)
 }
